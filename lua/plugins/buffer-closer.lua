476ec07d5fa9a313cf2d4c7ed242ca960c658a4e
gucchi0421 <gucchi.1999.1121.0421@gmail.com>
2025-03-14 19:17:40 +0900

    feat:  :sparkles:

--- lua/plugins/buffer-closer.lua
+++ lua/plugins/buffer-closer.lua
@@ -1,18 +1,5 @@
 return {
   {
-    "sontungexpt/buffer-closer",
-    event = "VeryLazy",
-    config = function()
-      require("buffer-closer").setup({
-        -- 最低6つのバッファを残す
-        min_remaining_buffers = 5,
-
-        -- 1分間はバッファを保持
-        retirement_minutes = 1,
-      })
-    end,
-  },
-  {
     "psjay/buffer-closer.nvim",
     config = function()
       require("buffer-closer").setup({
